778379b525fc99492d6e5ac6c203a6c7ee5a943d
Manuel Jerger <git@jerger.io>
2026-09-06 00:51:21 +0200

    fix battery status

--- battery.h
+++ battery.h
@@ -24,8 +24,8 @@ class Battery
 
     Battery() : 
       voltage_mV(0),
-      hysteresis_mV(200),
-      levels{{ BATT_CRITICAL,   "critical"  , 10000 },
+      hysteresis_mV(150),
+      levels{{ BATT_CRITICAL,   "critical"  , 11000 },
              { BATT_LOW,        "low"       , 11800 },
              { BATT_OK,         "ok"        , 13700 },
              { BATT_CHARGING,   "charging"  , 14400 },
@@ -43,32 +43,20 @@ class Battery
       return levels[status];
     }
 
-    const uint16_t getVoltage() {
-      return voltage_mV;
+    const float getVoltage() {
+      return (float)voltage_mV / 1000.0f;
     }
 
     const uint8_t getCharge() {
       uint32_t v = voltage_mV;
-      uint32_t p;
-
-      // Below 11.6V
-      if (v <= 11600) {
-          p = 0;
-      }
-      // 11.6V to 12.2V
-      else if (v <= 12200) {
-          p = ((v - 11600) * 50) / 600;
-      }
-      // 12.2V to 12.85V
-      else if (v <= 12850) {
-          p = 50 + ((v - 12200) * 50) / 650;
-      }
-      // Above 12.85V
-      else {
-          p = 100;
-      }
-
-      return (uint8_t)p;
+      if (v <= 11600)       // < 11.6V
+        return 0;
+      else if (v <= 12200)  // 11.6V to 12.2V
+        return ((v - 11600) * 50) / 600;
+      else if (v <= 12850)  // 12.2V to 12.85V
+        return 50 + ((v - 12200) * 50) / 650;
+      else                  // > 12.85V
+        return 100;
     }
 
     void updateVoltage(uint16_t mV) {
@@ -80,17 +68,15 @@ class Battery
 
       // find new level upwards
       bool changed = false;
-      while (s < 4 && mV > level.mV + hysteresis_mV) {
+      if (s < 4 && mV > levels[s].mV + hysteresis_mV) {
         s++;
         changed = true;
       }
 
       // find new level downwards
-      if (!changed) {
-        while (s > 0 && mV < level.mV - hysteresis_mV) {
-          s--;
-          changed = true;
-        }
+      if (!changed && s > 0 && mV < levels[s-1].mV - hysteresis_mV) {
+        s--;
+        changed = true;
       }
 
       if (changed) {
